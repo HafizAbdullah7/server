e3c7d9a4c3dc41e212337e24b5ea152de08f5c57
HafizAbdullah7 <131934473+HafizAbdullah7@users.noreply.github.com>
2023-10-02 11:58:02 -0700

    first commit

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ app.use(cors());
 
 app.get('/download', (req, res) => {
   const url = req.query.url;
+  res.header("Access-Control-Allow-Origin","*")
   ytdl(url, { filter: format => format.container === 'mp4' })
     .pipe(res);
 });
-
+app.get('/', (req, res) => {
+  res.send('Hello, this is the YouTube Video Downloader server!');
+});
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
 });
