0a257cec7e0cced1764581fcb83cf5540834a4f6
HafizAbdullah7 <131934473+HafizAbdullah7@users.noreply.github.com>
2023-10-15 10:46:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const ytdl = require('ytdl-core');
 const app = express();
 
 app.use(cors());
+app.get('/', (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin","true")
+  res.send('Hello, this is the YouTube Video Downloader server!');
+});
 
 app.get('/download', (req, res) => {
   const url = req.query.url;
-  res.setheader("Access-Control-Allow-Origin")
   ytdl(url, { filter: format => format.container === 'mp4' })
     .pipe(res);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello, this is the YouTube Video Downloader server!');
-});
+
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
 });
